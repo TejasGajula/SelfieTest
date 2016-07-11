cb8541d9352f877314d9f4c73ee2fcb93d0807b6
TejasGajula <tejasgajula@gmail.com>
2016-07-11 14:05:48 -0500

    Song added

--- app/src/main/java/com/example/tejas/selfietest/SelfieTestPostSecondActivity.java
+++ app/src/main/java/com/example/tejas/selfietest/SelfieTestPostSecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.tejas.selfietest;
 
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -14,5 +15,7 @@ public class SelfieTestPostSecondActivity extends AppCompatActivity {
         TextView welcomeMessage = (TextView) findViewById(R.id.JoinMessage);
         Typeface helvetica = Typeface.createFromAsset(getAssets(), "HelveticaNeue.ttf");
         welcomeMessage.setTypeface(helvetica);
+        MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.sariassong);
+        mediaPlayer.start();
     }
 }
