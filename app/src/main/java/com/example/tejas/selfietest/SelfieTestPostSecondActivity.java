bac5106cbd198f36118e86b4663f100c9f7a1a56
TejasGajula <tejasgajula@gmail.com>
2016-07-12 14:11:26 -0500

    facebook
    ye

--- app/src/main/java/com/example/tejas/selfietest/SelfieTestPostSecondActivity.java
+++ app/src/main/java/com/example/tejas/selfietest/SelfieTestPostSecondActivity.java
@@ -3,6 +3,7 @@ package com.example.tejas.selfietest;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareButton;
 
 public class SelfieTestPostSecondActivity extends AppCompatActivity {
 
@@ -18,11 +21,16 @@ public class SelfieTestPostSecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selfie_test_post_second);
+        ShareLinkContent content = new ShareLinkContent.Builder()
+                .setContentUrl(Uri.parse("https://developers.facebook.com"))
+                .build();
         TextView welcomeMessage = (TextView) findViewById(R.id.JoinMessage);
         Typeface helvetica = Typeface.createFromAsset(getAssets(), "HelveticaNeue.ttf");
         welcomeMessage.setTypeface(helvetica);
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(this);
+        ShareButton shareButton = (ShareButton)findViewById(R.id.shareButton);
+        shareButton.setShareContent(content);
         MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.sariassong);
         mediaPlayer.start();
     }
